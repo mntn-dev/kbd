415b4d8acf1e04f025c927c799e9be866c1aaca8
Benjamin Lips <g@mn.tn>
2018-04-14 15:09:50 +0200

    0.2

--- kbd.js
+++ kbd.js
@@ -1,4 +1,5 @@
-/*$.kbd-0.1;(c)2018 - Mntn(r) <https://mn.tn/> c/o Benjamin Lips <g--[AT]--mn.tn>;MIT-Licensed <https://mit-license.org/>;For documentation, see <https://github.com/mntn-dev/kbd/>*/
+
+/*$.kbd-0.2;(c)2018 - Mntn(r) <https://mn.tn/> c/o Benjamin Lips <g--[AT]--mn.tn>;MIT-Licensed <https://mit-license.org/>;For documentation, see <https://github.com/mntn-dev/kbd/>*/
 ;(function($){
 
 $.kbd=function(_){
@@ -9,7 +10,8 @@ $('body').append('<div id="_kbd"><span id="_kbd_wrap"></span>'+(_.coffee?'<div i
 if(_.white===!!1)$('#_kbd').addClass('_kbd_white');
 
 var _kbd_t=!1,q='',tc=function(_){c=_.data().c;cc=_.text();_.text(c);_.data('c',cc);},
-Q=function(_){q=$('#__q').text();return((_===0)?q.toLowerCase():q);};
+Q=function(_){q=$('#__q').text();return((_===0)?q.toLowerCase():q);},T=function(){return(window.ontouchstart!==undefined);},
+t_up=function(_){if(T())setTimeout(function(_){_.removeClass('_kbd-h'+($('#_kbd').hasClass('_kbd_white')?'-w':''));},1e2,_);}
 
 _k='q1w2e3r4t5y6u7i8o9p0__a@s#d$f_g&h-j+k(l)__{{\u25b2|data-c="\u25bc" class="l _caps"}}z/x%c*v,b"n!m?{{\u25c4|class="l" data-k="8"}}__{{123|data-c="ABC" class="l _alt"}}{{&nbsp;|class="xl" data-k="32"}}{{OK|class="l" data-k="13"}}';
 _e={};_e.q='1';_e.w='2';_e.r='4';_e.t='5';_e.y='6';_e.i='8';_e.p='0';_e.a='\u00e1\u00e0\u00e4';_e.e='\u00e9\u00e8\u00eb3';_e.o='\u00f3\u00f2\u00f69';_e.u='\u00fa\u00f9\u00fc7';_e.d='#\u00a5\u00a3\u20ac';_e.s='\u00df';_e.z='#\\';_e.k='#{[<';_e.l='#}]>';_e.v='#;:.';_e.b='#\'';
@@ -29,33 +31,47 @@ for(j=0;j<_k.length;j++){
 
 
 
-$('._caps')
-.mousedown(function(){
- if((q=Q()).toLowerCase()!='q')return;_kbd_t=setTimeout(function(_){_.addClass('_ext-caps');_.trigger('mouseup');_kbd_t='%';},1e3,$(this));
+$('._caps').bind('mousedown touchstart',function(_){
+ _.preventDefault();
+ if((q=Q()).toLowerCase()!='q')return;
+ if(T())$(this).trigger('mouseenter',!!1);
+ _kbd_t=setTimeout(function(_){_.addClass('_ext-caps');_.trigger('mouseup');_kbd_t='%';},750,$(this));
  })
-.mouseup(function(){
- if((q=Q()).toLowerCase()!='q'||_kbd_t=='%')return((_kbd_t=!1));
- tc($(this));if(q=='Q'&&$(this).hasClass('_ext-caps'))$(this).removeClass('_ext-caps');if(_kbd_t)clearTimeout(_kbd_t);
+.bind('mouseup touchend',function(_){
+ _.preventDefault();t_up($(this));
+ if((q=Q()).toLowerCase()!='q'||_kbd_t=='%')return((_kbd_t=!1));if(_kbd_t)clearTimeout(_kbd_t);
+ tc($(this));if(q=='Q'&&$(this).hasClass('_ext-caps'))$(this).removeClass('_ext-caps');
  C=(q=='Q')?!1:!!1;$('#_kbd button').each(function(){if($(this).data().a!==undefined||$(this).parent().hasClass('_ext-box')){$(this).text((C)?$(this).text().toUpperCase():(($(this).text()=='SS')?'\u00df':$(this).text().toLowerCase()));}});
 });
 
 $('._alt').click(function(){
- tc($(this));if(Q()=='Q'){tc($('._caps'));$('._caps').removeClass('_ext-caps');}
+ tc($(this));if(Q()=='Q'){tc($('._caps'));$('._caps').removeClass('_ext-caps');}if(T()){$(this).trigger('mouseenter',!!1);t_up($(this));}
  $('#_kbd button').each(function(){if((a=$(this).data().a)!==undefined){$(this).data('a',$(this).text().toLowerCase());$(this).text(a);}});
  $('._ext:not(._ext-0)').toggleClass('_ext-1');
-});
+}).bind('touchstart',function(){$(this).trigger('mouseenter',!!1);});
 
-$('button[data-a],button[data-k],._ext-box button').mouseup(function(_$,r){
+$(__='button[data-a],button[data-k],._ext-box button').bind('mouseup touchend',function(_$,r){
+ _$.preventDefault();
  if(_kbd_t&&!r){clearTimeout(_kbd_t);clearInterval(_kbd_t);}
  if((typeof(_.input))[0]=='f')_.input($(this).data().k?String.fromCharCode($(this).data().k):$(this).text());
- if(Q()=='Q'&&!$('._caps').hasClass('_ext-caps'))$('._caps').trigger('mouseup');
-});
+ if(Q()=='Q'&&!$('._caps').hasClass('_ext-caps'))$('._caps').trigger('mouseup');t_up($(this));
+}).bind('touchstart',function(){$(this).trigger('mouseenter',!!1);});
 
-$('button[data-k]').mousedown(function(){_kbd_t=setTimeout(function(_){_kbd_t=setInterval(function(){_.trigger('mouseup',!!1);},50,_);},2e2,$(this));});
+$(__+',._caps,._alt').
+hover(
+function(_,r){if(!T()||r!==undefined)$(this).addClass('_kbd-h'+($('#_kbd').hasClass('_kbd_white')?'-w':''));},
+function(){if(!T())$(this).removeClass('_kbd-h'+($('#_kbd').hasClass('_kbd_white')?'-w':''));}
+);
+
+
+$('button[data-k]').bind('mousedown touchstart',function(_){_.preventDefault();_kbd_t=setTimeout(function(_){_kbd_t=setInterval(function(){_.trigger('mouseup',!!1);},50,_);},2e2,$(this));});
 
 $('._ext')
-.mousedown(function(){if($(this).hasClass('_ext-1')||($(this).hasClass('_ext-0')&&Q(0)=='q'))_kbd_t=setTimeout(function(_){_.find('._ext-box').show();},750,$(this));})
-.on('mouseup mouseleave',function(){$(this).find('._ext-box:visible').hide();if(_kbd_t)clearTimeout(_kbd_t);});
+.bind('mousedown touchstart',function(_){_.preventDefault();if($(this).hasClass('_ext-1')||($(this).hasClass('_ext-0')&&Q(0)=='q')){_kbd_t=setTimeout(function(_){_.find('._ext-box').show();},750,$(this));if(T())setTimeout(function(_){_.find('._ext-box').hide();},3e3,$(this));  }})
+.bind('mouseup mouseleave',function(_){_.preventDefault();$(this).find('._ext-box:visible').hide();if(_kbd_t)clearTimeout(_kbd_t);});
+
+
+$('._ext-box button').bind('touchend',function(){$(this).parent().hide();});
 
 if(_.hidden===!!1)return;$.kbd.show();
 
